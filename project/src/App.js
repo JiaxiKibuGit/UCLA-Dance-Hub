116b56e9d09b91b7a13da83a5a35cdb2853b81e9
Jason Tran <jsontran5@gmail.com>
2023-12-03 03:11:13 -0800

    added sama and aca temp team pages

--- project/src/App.js
+++ project/src/App.js
@@ -6,23 +6,24 @@ import Map from './pages/map.js';
 import OrganizationSearch from './pages/organizationsearch.js';
 import NotFound from './pages/notfound.js';
 import Profile from './pages/profile.js';
-
+import ACAteamPage from './pages/ACAteamPage.js'; 
+import Samahangteampage from './pages/Samahangteampage.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <Router>
-        <Routes>
-          <Route exact path="/" element={<HomePage/>}/>
-          <Route exact path="/organizations" element={<OrganizationSearch/>}/>
-          <Route exact path="/map" element={<Map/>}/>
-          <Route exact path="/profile" element={<Profile/>}/>
-          <Route path="*" element={<NotFound/>}/>
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/organizations" element={<OrganizationSearch />} />
+        <Route exact path="/map" element={<Map />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/aca" element={<ACAteamPage />} /> {/* New route for ACA team page */}
+        <Route exact path="/samahang" element={<Samahangteampage />} /> {/* New route for Samahang team page */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
-}// make buttons to link /xyz to access other pages easily 
-     
-
+}
 
 export default App; 
