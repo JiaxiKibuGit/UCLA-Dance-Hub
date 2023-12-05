766bf31cb34bc18ef51fcc079c16b53447b3be92
Jiaxi Li <jiaxi@kibuhq.com>
2023-12-05 05:49:58 -0800

    fixed error on dbhelper

--- project/src/pages/components/dbHelper.js
+++ project/src/pages/components/dbHelper.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get } from "firebase/database";
+import { getDatabase, ref, get, set, push } from "firebase/database";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 //firebase config
@@ -13,7 +13,6 @@ export const firebaseConfig = {
   appId: "1:23121829941:web:a54c810e7e2c26763e517c",
   measurementId: "G-58SW1QF39G",
 };
-
 //initialize firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -32,9 +31,19 @@ export async function signIn() {
 export function PLHandler() {
   auth.onAuthStateChanged((user) => {
     if (user) {
-      window.location.replace("profile");
+      window.location.replace("/profile");
     } else {
       signIn();
+    }
+  });
+}
+
+//CHECK FOR AUTH TO USE HOSTING EVENT PAGE
+export function CheckIfAuth() {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+    } else {
+      window.location.replace("/NLI");
     }
   });
 }
@@ -56,7 +65,6 @@ export function useGetName() {
 
   return currentName;
 }
-
 export function useGetEmail() {
   const [currentEmail, setCurrentEmail] = useState("");
 
@@ -90,43 +98,137 @@ export function useGetPFP() {
 
   return currentPFP;
 }
+
 /* TEAM DATABASE READ FUNCTIONS */
 
-//1=NAME, 2=DESCRIPTION, 3=PHOTO, 4=MEMBER LIST
+//1=NAME, 2=DESCRIPTION, 3=PHOTO
 export function GetTeamInfo(teamid, choice) {
-  const [temp, setTemp] = useState("Loading");
-  const dbRef = ref(db, "/teams/" + teamid);
+  const [teamInfo, setTeamInfo] = useState("Loading");
 
-  get(dbRef).then((snapshot) => {
+  useEffect(() => {
+    const dbRef = ref(db, "/teams/" + teamid);
+
+    get(dbRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          let teamName;
+          switch (choice) {
+            case 1:
+              teamName = snapshot.val().team_name;
+              break;
+            case 2:
+              teamName = snapshot.val().about_me;
+              break;
+            case 3:
+              teamName = snapshot.val().key_image;
+              break;
+            default:
+              teamName = "error";
+          }
+          setTeamInfo(teamName);
+        } else {
+          console.log("Team not found.");
+          setTeamInfo("Team not found");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching team data:", error);
+        setTeamInfo("Error fetching team data");
+      });
+  }, [teamid, choice]);
+
+  return teamInfo;
+}
+
+// GET SEARCH LIST FOR TEAMS
+export async function GetSearchList() {
+  const dbRef = ref(db, "/teams/");
+  let team_and_members = [];
+
+  try {
+    const snapshot = await get(dbRef);
+
     if (snapshot.exists()) {
-      var teamName;
-      switch (choice) {
-        case 1:
-          teamName = snapshot.val().team_name;
-          break;
-        case 2:
-          teamName = snapshot.val().about_me;
-          break;
-        case 3:
-          teamName = snapshot.val().key_image;
-          break;
-        case 4:
-          teamName = snapshot.val().team_name;
-          break;
-        default:
-          teamName = "error";
+      let teamData = snapshot.val();
+      for (let i = 1; i < Object.keys(teamData).length + 1; i++) {
+        const name = teamData[i]["team_name"];
+        const team = i;
+        team_and_members.unshift({ name, team }); // add team:teamid
+
+        let members = teamData[i]["memberlist"];
+        for (const name in members) {
+          const team = members[name];
+          team_and_members.push({ name, team }); // add member:teamid
+        }
       }
-      //console.log(teamName);
-      setTemp(teamName);
     } else {
-      console.log("Team not found.");
-      setTemp("Team not found");
+      console.log("No data found at /teams/");
     }
-  });
-  return temp;
+  } catch (error) {
+    console.error("Error fetching team data:", error);
+  }
+  return team_and_members;
 }
 
-export function GetSearchTeam(name) {
-  return 1;
+//CREATE EVENT
+export function createEvent(eventData) {
+  const eventsRef = ref(db, "events");
+  return push(eventsRef, eventData)
+    .then((response) => {
+      console.log("Event created successfully.");
+      return response; // Return the response which contains the reference to the new event
+    })
+    .catch((error) => {
+      console.error("Error creating event: ", error);
+      throw error;
+    });
 }
 
+//GET EVENT
+export async function getEvent(eventId) {
+  const db = getDatabase();
+  const eventRef = ref(db, `events/${eventId}`);
+
+  try {
+    const snapshot = await get(eventRef);
+    if (snapshot.exists()) {
+      return snapshot.val();
+    } else {
+      console.log("Event not found");
+      return null; // or throw an error as per your error handling strategy
+    }
+  } catch (error) {
+    console.error("Error fetching event: ", error);
+    throw error; // Re-throw the error for handling in the calling component
+  }
+}
+
+//GET LIST OF EVENTS
+export async function GetEventList() {
+  const dbRef = ref(db, "/events/");
+  let eventInfo = [];
+  try {
+    const snapshot = await get(dbRef);
+    if (snapshot.exists()) {
+      let eventData = snapshot.val();
+      for (const i in eventData) {
+        const name = eventData[i]["name"];
+        const location = eventData[i]["location"];
+        const time = eventData[i]["startTime"];
+        const date = eventData[i]["startDate"];
+        const org = eventData[i]["organization"];
+        eventInfo.push({ name, location, date, time, org }); // add team:teamid
+        let members = eventData[i]["memberlist"];
+        for (const name in members) {
+          const team = members[name];
+          eventInfo.push({ name, team }); // add member:teamid
+        }
+      }
+    } else {
+      console.log("No data found at /teams/");
+    }
+  } catch (error) {
+    console.error("Error fetching team data:", error);
+  }
+  return eventInfo;
+}
