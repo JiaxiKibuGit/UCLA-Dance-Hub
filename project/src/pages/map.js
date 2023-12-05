672656867d57401c39b51c8332ad8a07b64d3e8f
Thomas McConnell <thomasmcconnell24@icloud.com>
2023-12-04 23:40:21 -0800

    added ucla map image and display it on map bar

--- project/src/pages/map.js
+++ project/src/pages/map.js
@@ -1,16 +1,31 @@
-
 import NavBar from './components/navbar';
 
 export default function Map() {
-    return (
-      <div>
-        <NavBar/>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>Map</p>
+  return (
+    <div>
+      <NavBar />
+      <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+        <img
+          src="map.png"
+          alt="UCLA Map"
+          style={{ width: '100%', height: '100%' }}
+        />
+        {/* Creating a resizable circle */}
+        <div
+          style={{
+            position: 'absolute',
+            bottom: '14%', // Adjust position from bottom
+            left: '30%', // Adjust position from left
+            width: '3%', // Adjust the relative size
+            paddingBottom: '2.75%', // Maintain aspect ratio for circle
+            borderRadius: '50%',
+            backgroundColor: 'black', // Change the inside color of the circle
+            backgroundImage: 'radial-gradient(circle at 50% 50%, #7ef542, #5ae813)', // Change the inside color using a gradient
+            border: '1.5px solid black', // Add a black border or outline to the circle
+            boxSizing: 'border-box', // Ensure the border size doesn't increase the circle's overall size
+          }}
+        ></div>
       </div>
-
-    );
-  }
-  
+    </div>
+  );
+}
