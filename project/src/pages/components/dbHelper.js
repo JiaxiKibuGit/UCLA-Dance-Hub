b5c54e8a1a0357bbb01a1ba4faef3969d1103da8
Jiaxi Li <jiaxi@kibuhq.com>
2023-12-05 05:32:14 -0800

    worked on a beta for the profile page

--- project/src/pages/components/dbHelper.js
+++ project/src/pages/components/dbHelper.js
@@ -1,112 +1,131 @@
-import React, {useState, useEffect} from 'react';
-import {initializeApp} from 'firebase/app';
-import {getDatabase, ref, get} from 'firebase/database';
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
-
+import React, { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, get } from "firebase/database";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 //firebase config
 export const firebaseConfig = {
-    apiKey: "AIzaSyATM-7r3rmpU6WOOPSpy1XU-db0Cj-nAW4",
-    authDomain: "ucladance-c16e6.firebaseapp.com",
-    projectId: "ucladance-c16e6",
-    storageBucket: "ucladance-c16e6.appspot.com",
-    messagingSenderId: "23121829941",
-    appId: "1:23121829941:web:a54c810e7e2c26763e517c",
-    measurementId: "G-58SW1QF39G"
+  apiKey: "AIzaSyATM-7r3rmpU6WOOPSpy1XU-db0Cj-nAW4",
+  authDomain: "ucladance-c16e6.firebaseapp.com",
+  projectId: "ucladance-c16e6",
+  storageBucket: "ucladance-c16e6.appspot.com",
+  messagingSenderId: "23121829941",
+  appId: "1:23121829941:web:a54c810e7e2c26763e517c",
+  measurementId: "G-58SW1QF39G",
 };
-  
+
 //initialize firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase();
 
-
-
 /* AUTHORIZATON HANDLERS */
-
 
 //Google Auth
 export async function signIn() {
-    const googleProvider = await new GoogleAuthProvider();
-    const res = await signInWithPopup(auth, googleProvider);
-    window.location.replace("/profile");
-};
-
+  const googleProvider = await new GoogleAuthProvider();
+  const res = await signInWithPopup(auth, googleProvider);
+  window.location.replace("/profile");
+}
 
 //go to signin if not logged in, go to profile
 export function PLHandler() {
-    auth.onAuthStateChanged((user) => {
-        if (user) {
-            window.location.replace("profile");
-        } else {
-            signIn();
-        }
-    });
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      window.location.replace("profile");
+    } else {
+      signIn();
+    }
+  });
 }
-
 
 //USER INFORMATON MODIFIER
 export function useGetName() {
-    const [currentName, setCurrentName] = useState("");
+  const [currentName, setCurrentName] = useState("");
 
-    useEffect(() => {
-        const x = auth.onAuthStateChanged(user => {
-            if (user) {
-                setCurrentName(user.displayName);
-            } else {
-                setCurrentName("Sign In");
-            }
-        });
-        return () => x();
-    }, []);
+  useEffect(() => {
+    const x = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentName(user.displayName);
+      } else {
+        setCurrentName("Sign In");
+      }
+    });
+    return () => x();
+  }, []);
 
-    return currentName;
+  return currentName;
 }
 
+export function useGetEmail() {
+  const [currentEmail, setCurrentEmail] = useState("");
 
+  useEffect(() => {
+    const x = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentEmail(user.email);
+      } else {
+        setCurrentEmail("Sign In");
+      }
+    });
+    return () => x();
+  }, []);
 
+  return currentEmail;
+}
 
+export function useGetPFP() {
+  const [currentPFP, setCurrentPFP] = useState("");
+
+  useEffect(() => {
+    const x = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentPFP(user.photoURL);
+      } else {
+        setCurrentPFP("Sign In");
+      }
+    });
+    return () => x();
+  }, []);
+
+  return currentPFP;
+}
 /* TEAM DATABASE READ FUNCTIONS */
 
 //1=NAME, 2=DESCRIPTION, 3=PHOTO, 4=MEMBER LIST
 export function GetTeamInfo(teamid, choice) {
-    const [temp, setTemp] = useState("Loading");
-    const dbRef = ref(db, '/teams/' + teamid);
+  const [temp, setTemp] = useState("Loading");
+  const dbRef = ref(db, "/teams/" + teamid);
 
-    get(dbRef)
-        .then((snapshot) => {
-            if (snapshot.exists()) {
-                var teamName;
-                switch(choice) {
-                    case 1:
-                        teamName = snapshot.val().team_name;
-                        break;
-                    case 2:
-                        teamName = snapshot.val().about_me;
-                        break;
-                    case 3:
-                        teamName = snapshot.val().key_image;      
-                        break;
-                    case 4:
-                        teamName = snapshot.val().team_name;
-                        break;
-                    default:
-                        teamName = "error"
-                }
-                //console.log(teamName);
-                setTemp(teamName);
-            } else {
-                console.log('Team not found.');
-                setTemp("Team not found");
-            }
-        })
-    return temp;
+  get(dbRef).then((snapshot) => {
+    if (snapshot.exists()) {
+      var teamName;
+      switch (choice) {
+        case 1:
+          teamName = snapshot.val().team_name;
+          break;
+        case 2:
+          teamName = snapshot.val().about_me;
+          break;
+        case 3:
+          teamName = snapshot.val().key_image;
+          break;
+        case 4:
+          teamName = snapshot.val().team_name;
+          break;
+        default:
+          teamName = "error";
+      }
+      //console.log(teamName);
+      setTemp(teamName);
+    } else {
+      console.log("Team not found.");
+      setTemp("Team not found");
+    }
+  });
+  return temp;
 }
-
-
 
 export function GetSearchTeam(name) {
-    return 1;
+  return 1;
 }
-
-
--- project/src/pages/profile.js
+++ project/src/pages/profile.js
@@ -1,17 +1,55 @@
 import React, { useEffect, useState } from "react";
-
 import NavBar from "./components/navbar.js";
-import { useGetName } from "./components/dbHelper.js";
+import { useGetEmail, useGetName, useGetPFP } from "./components/dbHelper.js";
+import { getAuth, signOut } from "firebase/auth";
 
 export default function Profile(props) {
+  const [name, setName] = useState(null);
+  const auth = getAuth(); // Assuming Firebase is initialized globally
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      window.location.replace("/");
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <NavBar />
-      <br></br>
-      <br></br>
-      <br></br>
-      <p>Profile</p>
-      <p>Name: {useGetName()}</p>
+      <br />
+      <br />
+      <br />
+      <p style={{ fontSize: "3rem", fontWeight: "bold" }}>Profile</p>
+      <div
+        style={{
+          background: "#ddd",
+          padding: "20px",
+          borderRadius: "8px",
+          display: "inline-block",
+        }}
+      >
+        <img
+          src={useGetPFP()}
+          alt="Profile"
+          style={{
+            width: "200px",
+            height: "200px",
+            objectFit: "cover",
+            borderRadius: "100%",
+          }}
+        />
+        <p style={{ fontSize: "1.5rem" }}>Name: {useGetName()}</p>
+        <p style={{ fontSize: "1.5rem" }}>Gmail: {useGetEmail()}</p>
+        <button
+          onClick={handleSignOut}
+          style={{ fontSize: "1.5rem", marginRight: "10px" }}
+        >
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 }
