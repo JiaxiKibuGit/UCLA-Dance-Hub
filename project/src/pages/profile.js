2807ce4e42c8ae31bdb90006a9adbe0195fac87c
leojxzhangg <leozhang@ucla.edu>
2023-12-05 00:16:27 -0800

    second bugfix
    forgot an import fix in the earlier bugfix, change that as well

--- project/src/pages/profile.js
+++ project/src/pages/profile.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 
 import NavBar from './components/navbar.js';
-import {GetName} from './components/dbHelper.js'
+import {useGetName} from './components/dbHelper.js'
 
 
 export default function Profile(props) {
