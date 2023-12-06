338ec7170d2fdf8953060ffa9d20f4d892aaf795
Jason Tran <jsontran5@gmail.com>
2023-12-06 03:09:05 -0800

    teampage follow button updates database

--- project/src/pages/components/dbHelper.js
+++ project/src/pages/components/dbHelper.js
@@ -26,19 +26,29 @@ const db = getDatabase();
 
 //Google Auth
 export async function signIn() {
+  const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
   try {
     const res = await signInWithPopup(auth, googleProvider);
     const user = res.user;
 
     // Check if user exists in the database, if not create a new user profile
+    const db = getDatabase();
     const userRef = ref(db, 'users/' + user.uid);
     get(userRef).then((snapshot) => {
       if (!snapshot.exists()) {
+        // Initialize 'orgs followed' with all teams set to false
+        const orgsFollowed = {
+          1: false, // Team ID 1
+          2: false, // Team ID 2
+          3: false, // Team ID 3
+          4: false, // Team ID 4
+          5: false  // Team ID 5
+        };
+
         set(userRef, {
           email: user.email,
-          'events followed': {},
-          'orgs followed': {}
+          'orgs followed': orgsFollowed
         });
       }
     });
--- project/src/pages/teampage.js
+++ project/src/pages/teampage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import NavBar from './components/navbar';
 import { GetTeamInfo } from './components/dbHelper.js';
-import './teampage.css'; 
-
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getDatabase, ref, update, get } from 'firebase/database';
+import './teampage.css';
 
 const TEAM_NAME = 1;
 const ABOUT_US = 2;
@@ -14,16 +15,16 @@ class TeamPage extends React.Component {
         super(props);
         this.state = {
             team_id: 1, // default value, can be updated based on sessionStorage or props
-            name:"", 
-            photo:"", 
-            about_us:"", 
-            video:"",
-            member1:[],
-            member2:[], 
-            member3:[],
-            member4:[],
+            name: "",
+            photo: "",
+            about_us: "",
+            video: "",
+            member1: [],
+            member2: [],
+            member3: [],
+            member4: [],
             isFollowing: false,
-
+            user: null,
         };
     }
 
@@ -39,10 +40,7 @@ class TeamPage extends React.Component {
         const tvideo = await GetTeamInfo(this.state.team_id, VIDEO);
         const tmembers = await GetTeamInfo(this.state.team_id, 5)
 
-        this.setState({ name: tname[0] });
-        this.setState({ photo: tphoto[0] });
-        this.setState({ about_us: tabout[0] });
-        this.setState({ video: tvideo[0] });
+        this.setState({ name: tname[0], photo: tphoto[0], about_us: tabout[0], video: tvideo[0] });
 
         let memberList = await Object.keys(tmembers[0]);
         var quarterSize = Math.floor(memberList.length / 4);
@@ -58,90 +56,98 @@ class TeamPage extends React.Component {
                 array4.push(memberList[i]);
             }
         }
-        await this.setState({ member1: array1 });
-        await this.setState({ member2: array2 });
-        await this.setState({ member3: array3 });
-        await this.setState({ member4: array4 });
+        this.setState({ member1: array1, member2: array2, member3: array3, member4: array4 });
 
+        // Check if the user is already following the team
+        const auth = getAuth();
+        onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                this.setState({ user: currentUser });
+                const db = getDatabase();
+                const userRef = ref(db, `users/${currentUser.uid}/orgs followed`);
+                get(userRef).then((snapshot) => {
+                    if (snapshot.exists() && snapshot.val()[this.state.team_id]) {
+                        this.setState({ isFollowing: true });
+                    }
+                });
+            }
+        });
     }
 
     handleFollowButtonClick = () => {
-        this.setState((prevState) => ({ isFollowing: !prevState.isFollowing }));
+        const { user, team_id, isFollowing } = this.state;
+        if (user) {
+            const db = getDatabase();
+            const userRef = ref(db, `users/${user.uid}/orgs followed`);
+            get(userRef).then((snapshot) => {
+                let orgsFollowedUpdate = {};
+                // Check if 'orgs followed' exists and update the specific team follow status
+                if (snapshot.exists()) {
+                    orgsFollowedUpdate[team_id] = !isFollowing;
+                } else {
+                    // If 'orgs followed' doesn't exist, create it with the specific team follow status
+                    orgsFollowedUpdate = { [team_id]: !isFollowing };
+                }
+                update(userRef, orgsFollowedUpdate);
+                this.setState({ isFollowing: !isFollowing });
+            });
+        } else {
+            console.log("User not logged in");
+        }
     }
 
     render() {
-        
-        const marginTopValue = '60px';
         const followButtonText = this.state.isFollowing ? 'Unfollow' : 'Follow';
 
         return (
             <div className="team-page-container">
                 <NavBar />
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
                 <div className="team-photo-container">
-                    <img
-                        src={this.state.photo}
-                        alt="Team Photo"
-                        className="team-photo"
-                    />
+                    <img src={this.state.photo} alt="Team Photo" className="team-photo" />
                 </div>
-
                 <div className="team-info-container">
-                    <h1 className="team-name">{this.state.name}
+                    <h1 className="team-name">
+                        {this.state.name}
                         <button className="follow-button" onClick={this.handleFollowButtonClick}>
                             {followButtonText}
                         </button>
                     </h1>
                     <h2>About Us</h2>
-                    <p className="about-us">
-                        {this.state.about_us}
-                    </p>
-
+                    <p className="about-us">{this.state.about_us}</p>
                     <h2>Sample Performance</h2>
                     <div className="video-container">
-                        <iframe
-                            src={this.state.video}
-                            title="YouTube video player"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowFullScreen
-                        ></iframe>
+                        <iframe src={this.state.video} title="YouTube video player" allowFullScreen></iframe>
                     </div>
-
                     <h2>Member List</h2>
-                    <div className = "aa">
+                    <div className="aa">
                         {this.state.member1.map((name, index) => (
-                        <div className="bb">
-                            <p>{name}</p>
-                        </div>
-                        ))}  
+                            <div className="bb">
+                                <p>{name}</p>
+                            </div>
+                        ))}
                         {this.state.member2.map((name, index) => (
-                        <div className="bb">
-                            <p>{name}</p>
-                        </div>
-                        ))}  
+                            <div className="bb">
+                                <p>{name}</p>
+                            </div>
+                        ))}
                         {this.state.member3.map((name, index) => (
-                        <div className="bb">
-                            <p>{name}</p>
-                        </div>
-                        ))}  
+                            <div className="bb">
+                                <p>{name}</p>
+                            </div>
+                        ))}
                         {this.state.member4.map((name, index) => (
-                        <div className="bb">
-                            <p>{name}</p>
-                        </div>
-                        ))}                                                                          
+                            <div className="bb">
+                                <p>{name}</p>
+                            </div>
+                        ))}
                     </div>
-  
-
-                    </div>
-
-                {/* Additional sections for team page */}
-                {/* ... */}
+                </div>
             </div>
         );
     }
 }
 
 export default TeamPage;
+
+
+
