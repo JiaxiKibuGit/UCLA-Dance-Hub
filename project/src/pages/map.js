4eef7dc30bdf89ae2d2f7e9eaf41d5d7cfdbbdc3
Thomas McConnell <thomasmcconnell24@icloud.com>
2023-12-06 01:42:49 -0800

    Markers now only appear if there is an upcoming event for that location. On hover they display the name of the event which is most soon. Lot 4 P1 and Lot 4 P2 now share a marker and thus show the most soon event between the two. New location Bruin Plaza added.

--- project/src/pages/map.js
+++ project/src/pages/map.js
@@ -1,41 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './components/navbar';
 import './map.css';
+import { GetEventList } from './components/dbHelper';
 
 export default function Map() {
+  console.log("don");
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const eventList = await GetEventList();
+      eventList.sort((a, b) => a.unix - b.unix); // Sort by upcoming date
+      setEvents(eventList);
+    };
+
+    fetchEvents();
+  }, []);
+
+  function mapLocationToMarkerId(location) {
+    const mapping = {
+      "Lot DD": 1,
+      "Lot 8 P2": 2,
+      "Pauley Pavillion (LATC)": 3,
+      "Lot 7 P1": 4,
+      "Wooden": 5,
+      "Bruin Plaza": 6,
+      "Lot 4 P1": 7,
+      "Lot 4 P2": 7
+    };
+    return mapping[location] || null; // Return the corresponding marker ID or null if no match
+  }
+
   const markers = [
     { id: 1, top: '27%', left: '21%'},
-    { id: 2, top: '34.5%', left: '53.5%' },
+    { id: 2, top: '70%', left: '30%' },
     { id: 3, top: '38.5%', left: '37.4%' },
-    { id: 4, top: '70%', left: '30%' },
-    { id: 5, top: '28%', left: '76.4%' },
-    { id: 6, top: '54%', left: '60%' }
+    { id: 4, top: '34.5%', left: '53.5%' },
+    { id: 5, top: '37%', left: '65.8%' },
+    { id: 6, top: '48.5%', left: '72.1%' },
+    { id: 7, top: '28%', left: '76.4%' },
+
   ];
+
+  const getEventForMarker = (markerId) => {
+    const today = Math.floor(Date.now() / 1000);
+  
+    const eventForMarker = events.find(event => {
+      const eventMarkerId = mapLocationToMarkerId(event.location);
+      return eventMarkerId === markerId && event.unix >= today;
+    });
+  
+    return eventForMarker;
+  }  
+  
 
   return (
     <div className="content-container">
       <NavBar />
       <br />
       <br />
-      <div className="centered-container"> {/* New container for centering */}
+      <div className="centered-container">
         <div className="map-container">
           <img
             src="map.png"
             alt="UCLA Map"
             className="responsive-map"
           />
-          {markers.map(marker => (
-            <div
-              key={marker.id}
-              className="tear"
-              style={{ top: marker.top, left: marker.left }}
-            >
-              <div className="popup">
-                <img src={marker.imageUrl} alt="Marker Image" className="popup-image" />
-                <p>{marker.info}</p>
+          {markers.map(marker => {
+            const event = getEventForMarker(marker.id);
+            return event ? (
+              <div
+                key={marker.id}
+                className="tear"
+                style={{ top: marker.top, left: marker.left }}
+              >
+                <div className="popup">
+                  <img src={event.imageUrl} alt="Marker Image" className="popup-image" />
+                  <p>{event.name} - {event.startDate}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ) : null;
+          })}
         </div>
       </div>
     </div>
